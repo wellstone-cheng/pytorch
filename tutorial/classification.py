8094f0c7a9a076df64658f4eb5d000ecfafd1e01
wellstone_cheng <wellstone_cheng@qq.com>
2020-04-11 06:20:14 +0800

    Add CNN classification

--- tutorial/classification.py
+++ tutorial/classification.py
@@ -10,15 +10,16 @@ x0 = torch.normal(2*n_data, 1)      # class0 x data (tensor), shape=(100, 2)
 y0 = torch.zeros(100)               # class0 y data (tensor), shape=(100, 1)
 x1 = torch.normal(-2*n_data, 1)     # class1 x data (tensor), shape=(100, 2)
 y1 = torch.ones(100)                # class1 y data (tensor), shape=(100, 1)
-x = torch.cat((x0, x1), 0).type(torch.FloatTensor)  # shape (200, 2) FloatTensor = 32-bit floating
-y = torch.cat((y0, y1), ).type(torch.LongTensor)    # shape (200,) LongTensor = 64-bit integer
+x = torch.cat((x0, x1), 0).type(torch.FloatTensor)  # shape (200, 2) FloatTensor = 32-bit floating　　按维数0拼接（竖着拼）
+y = torch.cat((y0, y1), ).type(torch.LongTensor)    # shape (200,) LongTensor = 64-bit integer  y表示数据标签
 
 # The code below is deprecated in Pytorch 0.4. Now, autograd directly supports tensors
 # x, y = Variable(x), Variable(y)
 
 # plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=y.data.numpy(), s=100, lw=0, cmap='RdYlGn')
 # plt.show()
-
+#
+print('x.data.numpy', x.data.numpy())
 
 class Net(torch.nn.Module):
     def __init__(self, n_feature, n_hidden, n_output):
